8c1937b22a968f89d777948c1a8edc2338b5f8b9
Camilo Ramírez <camiloram18042003@gmail.com>
2025-04-17 11:41:22 -0300

    [REMOVE]: remove debug log for token in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,6 @@ export function middleware(request) {
   const isAdminPage = request.nextUrl.pathname.startsWith('/admin')
   const isLoginPage = request.nextUrl.pathname === '/admin/login'
   const token = request.cookies.get('token')
-  console.log('Token:', token)
 
   if (isAdminPage) {
     if (!token && !isLoginPage) {
